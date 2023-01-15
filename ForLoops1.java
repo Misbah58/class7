d2046e1bc0c2f1f55db947501e778b8e99266f07
Misbah58 <afsarmisbah@gmail.com>
2023-01-14 22:00:23 -0500

    class7

---
+++ ForLoops1.java
@@ -0,0 +1,19 @@
+package class7;
+
+public class ForLoops1 {
+    public static void main(String[] args) {
+        // using a for loop print odd numbers from 1 to 20,
+        for (int i = 0; i < 20; i++) { // if want even number instead of ! should use ==.
+            if(i%2!=0){
+            System.out.println(i);
+            
+        }}
+
+
+            for (int j = 1; j <20 ; j+=2) {
+                System.out.println(j);
+            }
+                
+            }
+    }
+
---
+++ WhileLoops.java
@@ -0,0 +1,22 @@
+package class7;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        int number=0;
+        while (number<10){
+        System.out.println(number);
+        number++;
+
+    }
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+        for(int i=0;i<=20;i+=3){
+            System.out.println(i);
+
+
+
+
+        }
+
+}}
